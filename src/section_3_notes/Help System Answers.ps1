3b487c71fe5f292521144f362a0eb4a79090fbf8
Anthony Hudspeth <hudspeth199@gmail.com>
2022-11-30 18:48:28 -0600

    Section 3
    Answers for Question 2

--- src/section_3_notes/Help System Answers.ps1
+++ src/section_3_notes/Help System Answers.ps1
@@ -8,3 +8,16 @@ Get-Service "WinRM" -RequiredServices
 
 # Question 2 
 # Get-Eventlog -List List all the logs that are on the computer
+
+Get-EventLog -List 
+# Now get the 20 most recent entries from the system log
+
+# Either of these answers work
+Get-EventLog -LogName System -Newest 20
+Get-EventLog -Newest 20 -LogName System
+
+################################################################
+
+# Question 3 
+# The get-command, gets all the commands on the computer we want to get all the commands of all types
+
