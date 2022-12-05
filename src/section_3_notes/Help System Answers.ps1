e74bc75005590ddb0b600829c35391320388b3f9
Anthony Hudspeth <hudspeth199@gmail.com>
2022-12-04 19:21:48 -0600

    Update Help System Answers.ps1
    Question 4 and 5

--- src/section_3_notes/Help System Answers.ps1
+++ src/section_3_notes/Help System Answers.ps1
@@ -29,3 +29,26 @@ Get-Content
 # Open notepad -> create a few lines-> save it to your Content directory-> name it text.txt
 # Use the get-content command to the contents of your text file and display it to the console
 
+Get-Content -Path C:\Content\Text.txt
+#################################################################################
+
+# Question 5
+Clear-History
+# From the console pane type the command get-history
+# Displayed are all the commands that you have typed in powershell
+# Use the clear history command and only delete the first and last command that you typed into powershell
+# Now delete all remaining commands 
+
+Get-History
+
+Clear-History -Id 1,6
+Clear-History
+################################################################################################
+
+# Question 6
+Get-Alias
+# Using this command how can you figure out the alias for get-service
+Get-Help Get-Alias -ShowWindow
+
+Get-Alias -Definition Get-Service
+
