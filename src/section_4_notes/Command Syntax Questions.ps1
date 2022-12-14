1a128dcc554115d2c9ea59436fbbf975367e9ea1
Anthony Hudspeth <hudspeth199@gmail.com>
2022-12-13 18:24:09 -0600

    Command Syntax Question

---
+++ src/section_4_notes/Command Syntax Questions.ps1
@@ -0,0 +1,17 @@
+# Command Syntax Questions
+
+# Question one is in six parts start from the left and work to the right 
+Syntax
+    Get-Service [-ComputerName <System.String[]>] [-DependentServices ] -DisplayName <System.String[]> [-Exclude <System.String[]>] 
+    [-Include <System.String[]>] [-RequiredServices ] [<CommonParameters>]
+
+    Get-Service [-ComputerName <System.String[]>] [-DependentServices ] [-Exclude <System.String[]>] [-Include <System.String[]>]
+    [-InputObject <System.ServiceProcess.ServiceController[]>] [-RequiredServices ] [<CommonParameters>]
+
+    Get-Service [[-Name] <System.String[]>] [-ComputerName <System.String[]>] [-DependentServices ] [-Exclude <System.String[]>] 
+    [-Include <System.String[]>] [-RequiredServices ] [<CommonParameters>]
+
+# Number one, every command that is based upon the same structure, a name for this structure is
+
+# Number two, 
+# a dash tells Power Shell that A is being used.
