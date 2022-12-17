5d262db768e45da0298a2395cc73e2778c0a8b52
Anthony Hudspeth <hudspeth199@gmail.com>
2022-12-17 07:58:01 -0600

    Update Command Syntax Questions.ps1

--- src/section_4_notes/Command Syntax Questions.ps1
+++ src/section_4_notes/Command Syntax Questions.ps1
@@ -12,10 +12,23 @@ Syntax
     [-Include <System.String[]>] [-RequiredServices ] [<CommonParameters>]
 
 # Number one, 
-# every command that is based upon the same structure, a name for this structure is
+# every command that is based upon the same structure, a name for this structure is ____
 ## Syntax
 
 # Number two, 
-# a dash tells Power Shell that A is being used.
-## switch operators
+# a dash tells Power Shell that a ____ is being used.
+## switch operator
 
+# Number three
+# two angle brackets tells me this is a ____
+
+
+# Number four
+# the word string is located between two angle brackets, a string is called a ___
+
+# Number five
+# when you see two square brackets surrounded by two angle brackets, this tells you that this is a _____
+## double entry
+
+# Number six
+# These three groups are called ____
