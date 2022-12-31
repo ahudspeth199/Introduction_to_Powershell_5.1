0aa598d4d33444a90c474858d83fd37fe18dbaca
Anthony Hudspeth <hudspeth199@gmail.com>
2022-12-31 09:56:09 -0600

    Create Objects-Properties_Methods_pt1.ps1

---
+++ src/section_4_notes/Objects-Properties_Methods_pt1.ps1
@@ -0,0 +1,35 @@
+Objects, Properties and Methods
+Part 1 Objects
+The use of Objects is probably one of the most confusing parts of Powershell. By using
+illustrations or word pictures we hope to clarify PowerShells use of objects. If you have never
+programmed or scripted with objects, it’s critical that you gain an understanding of objects. 
+
+Objects
+The Boat
+• 1) So what are objects? Objects are things that we can interact with. For example: Like a
+boat or a car. Technically an object is the programmatic representation of anything.
+• An object is a cmdlet created by a developer.
+• 2) So why use a boat to illustrate an object? Because a boat is something that we have
+all seen and we all understand.
+• 3) Objects are usually considered as two types of things, Properties and Methods.
+• 4) Let’s take a look at some of these properties of our boat.
+• 5) Properties describe features, so let’s checkout some of these features.
+• 6) There’s our spot light, life preserver, COMS device, windows, and our engine.
+• 7) Now let’s take a look at Methods. Methods describe actions or in this example things
+that we can do with our boat.
+• 8) Like Turn on the light, Throw the life preserver, track ships with our communication
+device, clean the windows and start the engine.
+• 9) Here’s another way to show properties and methods.
+The Car – Let’s take a look at another example.
+• 10) We already said that an object can be a car.
+• 11) And that Objects consist of Properties and Methods.
+Properties describe the features of an object
+
+Let’s take a look at some of the features of our car
+Like headlights, wheels, side mirrors, our steering wheel, spoiler, and the door
+handle.
+2
+• 14. Methods describe actions or things that we can do with our car
+• 15. Like turn-on lights, add air to the tires, check for traffic in our side mirror, turn left
+using our steering wheel. Clean the dirt off our spoiler. And use the door handle to open
+the door.
